4667416dbda18b938e2cea2623ebefb11898f512
Kailc <1553207267@qq.com>
2017-10-05 15:21:46 +0800

    Initial commit

---
+++ tcp_client.py
@@ -0,0 +1,22 @@
+import socket
+
+target_host = "www.baidu.com"
+target_port = 80
+get_size = 4096
+
+# build a socket object
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# make a connect
+client.connect((target_host,target_port))
+
+# send some data
+client.send("GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n")
+
+# get data
+response = client.recv(get_size)
+
+print "these are the responses:"
+print ">>>>>>>>>>>>>>"
+
+print response
